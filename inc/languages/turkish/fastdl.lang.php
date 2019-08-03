0f980934fb23776245f51ba7300c1a96089602fb
Kerem <mail@keremer.com.tr>
2019-08-03 04:09:43 +0300

    Create fastdl.lang.php

---
+++ inc/languages/turkish/fastdl.lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$l['fastdl'] = 'Dosya İndirme Servisi';
+$l['haritalar'] = 'Haritalar';
+$l['harita'] = 'Harita';
+$l['boyut'] = 'Boyut';
+$l['indirmekicin'] = 'Indirmek için tıkla';
+$l['nasilyapilir'] = 'Nasıl Yapılır?';
+$l['nasilyapilir_aciklama'] = "<small><span class='text-secondary'>Haritayı indirdikten sonra</span>
+<ol class='pl-3'>
+    <li>İndirdiğiniz öğenin içerisinde ki <span class='text-muted'>[harita-adı]</span>.bsp adlı belgeyi masaüstüne çıkartın.</li>
+<li><i class='fab fa-steam'></i> Steam programını çalıştırarak <b>Kütüphane</b> bölümüne gelin.</li>
+<li>Soldaki listeden Team Fortress 2'yi bulun.</li>
+    <li>Team Fortress 2 üzerine sağ tıklayarak <b>Özelllikler</b>'i açın.</li>
+    <li>Açılan pencereden <b>Yerel Dosyalar</b> sekmesine geçin.</li>
+    <li><b>Yerel dosyalara göz at...</b> düğmesine tıklayın.</li>
+    <li>Masaüstüne çıkardığınız belgeyi <b>tf</b> klasörü içindeki <b class='text-primary'>maps</b> klasörüne kopyalayın.</li>
+    </ol>
+        </small>";
+$l['harita_adi'] = 'Harita Adı:';
